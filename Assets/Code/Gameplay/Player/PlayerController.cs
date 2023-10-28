ee15c5bf87f8113c7e4dcd96d879414f08c0cb8d
Sabekk <skb3@op.pl>
2023-10-28 15:45:58 +0200

    Fixed running and falling animations. Added following camera

--- Assets/Code/Gameplay/Player/PlayerController.cs
+++ Assets/Code/Gameplay/Player/PlayerController.cs
@@ -7,19 +7,20 @@ public class PlayerController : MonoBehaviour {
 	[SerializeField] float speed;
 	[SerializeField] float jumpPower;
 	[SerializeField] float gravity;
-	Rigidbody2D rb;
+
+	SpriteRenderer bodySprite;
 	Animator animator;
+	Rigidbody2D rb;
 	BoxCollider2D playerCollider;
 	bool isRightDirection;
 	MovingState currentState;
 	Vector2 direction = Vector2.zero;
-	Quaternion leftDirection = new Quaternion (0, 90, 0, 0);
-	Quaternion rightDirection = new Quaternion (0, 0, 0, 0);
 
-	public bool IsGrounded => Physics2D.BoxCast (transform.position, playerCollider.size, 0, Vector2.down, playerCollider.bounds.extents.y + 0.2f, LayerMask.GetMask("ground"));
+	public bool IsGrounded => Physics2D.BoxCast (transform.position, playerCollider.size, 0, Vector2.down, playerCollider.bounds.extents.y + 0.2f, LayerMask.GetMask ("ground"));
 
 
 	private void Awake () {
+		bodySprite = GetComponent<SpriteRenderer> ();
 		rb = GetComponent<Rigidbody2D> ();
 		animator = GetComponent<Animator> ();
 		playerCollider = GetComponent<BoxCollider2D> ();
@@ -44,12 +45,12 @@ public class PlayerController : MonoBehaviour {
 		if (direction.x > 0) {
 			if (!isRightDirection) {
 				isRightDirection = true;
-				transform.rotation = rightDirection;
+				bodySprite.flipX = false;
 			}
 		} else if (direction.x < 0) {
 			if (isRightDirection) {
 				isRightDirection = false;
-				transform.rotation = leftDirection;
+				bodySprite.flipX = true;
 			}
 		}
 	}
@@ -60,13 +61,21 @@ public class PlayerController : MonoBehaviour {
 	}
 
 	void UpdateState () {
-		if (rb.velocity.y > 0)
-			currentState = MovingState.jumping;
-		else if (rb.velocity.y < 0)
-			currentState = MovingState.falling;
-		else {
-			if (direction.x > 0 || direction.x < 0)
+		if (!IsGrounded) {
+			if (rb.velocity.y > 1.5f) {
+				currentState = MovingState.jumping;
+				Debug.Log ("jumping");
+			}
+			else if (rb.velocity.y < 1.5f) {
+				Debug.Log ("falling");
+				currentState = MovingState.falling;
+
+			}
+		} else {
+			if (direction.x > 0 || direction.x < 0) {
 				currentState = MovingState.running;
+				Debug.Log ("running");
+			}
 			else
 				currentState = MovingState.idle;
 		}
